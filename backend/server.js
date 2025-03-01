49218997b89bb9b9754436c5816b697e4d698b4f
JashawnRogers <rogersjashawn20@gmail.com>
2025-03-01 11:36:07 -0700

    Added support for CORS

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,20 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) // Handles form data
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    )
+    next()
+})
+
 app.use('/api/v1', v1Router)
 
 dbConnect()
